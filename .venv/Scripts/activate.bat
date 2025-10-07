29d831a80fd5a615451f9d41dad17699e56bbcc1
Estevão Lumingu <estevaolumingo@gmail.com>
2025-10-07 17:03:05 +0100

    Versão estável com BD restaurada

--- .venv/Scripts/activate.bat
+++ .venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\INFORBAZ\Desktop\FLUBRIENGLISH\.venv
+set VIRTUAL_ENV=C:\Users\rossa\Desktop\En. Macundi\Projects\FLUBRIENGLISH\.venv
 
 if not defined PROMPT set PROMPT=$P$G
 
